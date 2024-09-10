042abd032ffc40be3d662a2d26de46bc97fa9fc3
ArezooZafardoost <arezoo.zafardoost@gmail.com>
2024-09-10 10:42:29 +0330

    homework

---
+++ 2-1/script1.js
@@ -0,0 +1,20 @@
+// تابعی بنویسید که به عنوان ورودی یک ارایه بگیرد و تایپ تمام اعضای ارایه را برگرداند.
+
+function typer(hello) {
+
+    for (let i=0; i<hello.length; i++){
+
+
+        console.log(typeof hello[i]);
+
+        
+
+    } 
+
+    }
+
+ typer([1,"hi",true,{name:"arezoo"},null])
+
+
+ 
+
---
+++ 2-2/script2.js
@@ -0,0 +1,27 @@
+// تایپ داده های زیر را در کنسول چاپ کنید
+
+
+let num = 4+"4";
+console.log(typeof num);
+
+let hom = 2222n;
+console.log( typeof hom);
+
+let word = ("reza");
+console.log(typeof word);
+
+let num1 = 12 && 124;
+console.log(typeof num1);
+
+
+let num2 = 12 || 124;
+console.log(typeof num2);
+
+let not = null;
+console.log(typeof not);
+
+let num3 = 2*2;
+console.log(typeof num3);
+
+let num4 = 2*"2";
+console.log(typeof num4);
