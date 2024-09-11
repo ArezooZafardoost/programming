84f9d8cf509570eaba75082622bfbea6dda12001
ArezooZafardoost <arezoo.zafardoost@gmail.com>
2024-09-11 18:57:36 +0330

    exercise

---
+++ 2-4/script4.js
@@ -0,0 +1,8 @@
+// برنامه ای بنویسید که یک رشته (رضا)از ورودی دریافت کند و معکوس ان را برگرداند.
+
+const name = "رضا";
+
+let reverseName = 
+name.split('').reverse().join('');
+
+console.log("رشته معکوس شده"+reverseName )
