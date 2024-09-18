f5c99c877e0aac5c0ffaa050137ed042adadc542
ArezooZafardoost <arezoo.zafardoost@gmail.com>
2024-09-18 12:15:00 +0330

    exercise

--- 2-3/script3.js
+++ 2-3/script3.js
@@ -1,33 +1,39 @@
 // برنامه ای بنویسید که سن کاربر را دریافت کند و بررسی کند ایا ورودی صحیح است یا خیر سپس با alert به کاربر نشان دهد.
-// let age = prompt ('enter your age')
-// let age=20
+let age = prompt ('enter your age')
 
-// if (age>=20) {
-//     console.log("ورودی معتبر است");
+if (age>=20) {
+    console.log("ورودی معتبر است");
     
-// }else(age>=20) 
-//     console.log(" نامعتبر است");
+}else(age<=20) 
+    console.log(" نامعتبر است");
 
 
 
 
-let age= prompt('لطفا سن خود را وارد کنید');
+// let age= prompt('لطفا سن خود را وارد کنید');
 
-if (age === null || age.trim() === "" ) {
+// if (age === null || age.trim() === "" ) {
 
-    alert("ورودی خالی است لطفا سن را وارد کنید");
+//     alert("ورودی خالی است لطفا سن را وارد کنید");
 
-}else if (Number(age)===20) {
+// }else if (Number(age)===20) {
 
-    alert("سن شما 20 است.ورودی صحیح است");
+//     alert("سن شما 20 است.ورودی صحیح است");
         
-    }else{
-        alert("ورودی عدد است اما عدد وارد شده 20 نیست");
+//     }else{
+//         alert("ورودی عدد است اما عدد وارد شده 20 نیست");
+//     }
+
+
+   let x = prompt();
+   
+   if (x===Number){
+       console.log("معتبر");
+
+   }
+    else{
+        console.log("نامعتبر");
     }
-
-
-    
-    
 
 
 
